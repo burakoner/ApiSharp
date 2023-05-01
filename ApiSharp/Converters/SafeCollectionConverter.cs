569466f8ecd151c77b9a046883e734f1d7305788
Burak Öner <info@burakoner.com>
2023-05-01 15:16:39 +0300

    Version 1.3.3

---
+++ ApiSharp/Converters/SafeCollectionConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace ApiSharp.Converters;
+
+public class SafeCollectionConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return true;
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        // This not works for Populate (on existingValue)
+        return serializer.Deserialize<JToken>(reader).ToObjectCollectionSafe(objectType, serializer);
+    }
+
+    public override bool CanWrite => false;
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
